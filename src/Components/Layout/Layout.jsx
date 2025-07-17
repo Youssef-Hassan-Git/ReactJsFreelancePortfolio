1858050e25ce9089117a2f7fe48cf483823cdc26
Youssef Hassan <youssefhassan7666@gmail.com>
2025-07-17 04:02:51 +0300

    Scroll

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -7,12 +7,14 @@ import OfferedServices from '../OfferedServices/OfferedServices'
 import Contact from '../Contact/Contact'
 import Navbar from '../Navbar/Navbar'
 import Footer from '../Footer/Footer'
+import ScrollTop from '../Scroll/ScrollTop'
 
 export default function Layout() {
   return (
     <>
       <Navbar />
     <div className="bg-black">
+      <ScrollTop />
       <AboutMe />
       <Education />
       <Skills />
